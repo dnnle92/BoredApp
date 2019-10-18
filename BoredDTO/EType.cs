743d63fd400d7318923fed36e90075689fe8cc3e
dnnle92 <33842408+dnnle92@users.noreply.github.com>
2019-10-18 12:29:47 -0400

    Inital commit. Created BoredApi project and BoredDTO with Response class.

---
+++ BoredDTO/EType.cs
@@ -0,0 +1,15 @@
+﻿namespace BoredDTO
+{
+    public enum EType : byte
+    {
+        Education = 1, 
+        Recreational = 2,
+        Social = 3,
+        DIY = 4,
+        Charity = 5,
+        Cooking = 6,
+        Relaxation = 7,
+        Music = 8,
+        Busywork = 9
+    }
+}
---
+++ BoredDTO/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoredDTO
+{
+    class Response
+    {
+        public string Activity { get; set; }
+        public double Accessibility { get; set; }
+        public EType Type { get; set; }
+        public int Participants { get; set; }
+        public double Price { get; set; }
+    }
+}
