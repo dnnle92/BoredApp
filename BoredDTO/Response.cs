3b57d52ac93bcbc5d1af6a45881749b1db6272de
dnnle92 <33842408+dnnle92@users.noreply.github.com>
2019-10-18 18:30:21 -0400

    Added enum for Price

--- BoredDTO/Response.cs
+++ BoredDTO/Response.cs
@@ -4,13 +4,31 @@ using System.Text;
 
 namespace BoredDTO
 {
+    public enum EType
+    {
+        Education,
+        Recreational,
+        Social,
+        DIY,
+        Charity,
+        Cooking,
+        Relaxation,
+        Music,
+        Busywork
+    }
+
+    public enum EPrice
+    {
+        Free, Cheap, Moderate, Expensive
+    }
+
     public class Response
     {
         public int Id { get; set; }
         public string Activity { get; set; }
         public float Accessibility { get; set; }
-        public EType Type { get; set; }
+        public EType? Type { get; set; }
         public int Participants { get; set; }
-        public float Price { get; set; }
-    }
+        public EPrice? Price { get; set; }
+      }
 }
